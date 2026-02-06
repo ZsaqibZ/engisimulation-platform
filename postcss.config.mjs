1d6637ce0f17925b1c96f2a43a826c2b04bd9796
Saqib Hussain <saqibh573@gmail.com>
2026-02-06 20:18:30 +0500

    Git:Commit

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {}, // This line requires the package you just installed
-  },
-};
-
-export default config;
